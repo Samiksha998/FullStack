2fe7523dfc24aeb6d734e2109ca6ed5120b8ba1f
Samiksha Gurav <110155865+Samiksha998@users.noreply.github.com>
2025-05-03 21:14:06 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,21 @@
-variable "vpc_cidr_block" {}
-variable "private_subnet_cidr_blocks" {}
-variable "public_subnet_cidr_blocks" {}
+variable "env_prefix" {
+  type        = string
+  description = "Environment prefix like dev, prod"
+}
+
+variable "eks_cluster_name" {
+  type        = string
+  description = "EKS Cluster name"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+}
+
+variable "private_subnet_cidr_blocks" {
+  type        = list(string)
+}
+
+variable "public_subnet_cidr_blocks" {
+  type        = list(string)
+}
