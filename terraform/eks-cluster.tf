6ed9f7730760249edb88b52e4a6b78eb45c471eb
Samiksha Gurav <110155865+Samiksha998@users.noreply.github.com>
2025-05-08 16:25:45 +0530

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,26 +1,31 @@
 module "eks" {
-    source  = "terraform-aws-modules/eks/aws"
-    version = "~> 19.0"
-    cluster_name = "Fullstack-cluster"
-    cluster_version = "1.28"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
 
-    cluster_endpoint_public_access  = true
+  cluster_name    = "Fullstack-cluster"
+  cluster_version = "1.28"
 
-    vpc_id = module.myapp-vpc.vpc_id
-    subnet_ids = module.myapp-vpc.private_subnets
+  # Allow public access to EKS API server
+  cluster_endpoint_public_access = true
 
-    tags = {
-        environment = "development"
-        application = "myapp"
+  # ✅ Use public subnets instead of private ones
+  vpc_id     = module.myapp-vpc.vpc_id
+  subnet_ids = module.myapp-vpc.public_subnets
+
+  tags = {
+    environment = "development"
+    application = "myapp"
+  }
+
+  eks_managed_node_groups = {
+    dev = {
+      min_size       = 1
+      max_size       = 1
+      desired_size   = 1
+      instance_types = ["t2.medium"]
+
+      # Optional: ensure nodes get public IPs if subnets don't auto-assign them
+      # associate_public_ip_address = true
     }
-
-    eks_managed_node_groups = {
-        dev = {
-            min_size = 1
-            max_size = 1
-            desired_size = 1
-
-            instance_types = ["t2.medium"]
-        }
-    }
+  }
 }
